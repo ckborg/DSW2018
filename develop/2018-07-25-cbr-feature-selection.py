c3b20fcde34cbd9022a251d62ff407f887aaca62
Christopher Bronner <christopher.bronner@gmail.com>
2018-07-26 20:17:35 -0700

    use only samples that have density value

--- develop/2018-07-25-cbr-feature-selection.py
+++ develop/2018-07-25-cbr-feature-selection.py
@@ -1,10 +1,12 @@
 
 # coding: utf-8
 
-# In[14]:
+# In[87]:
 
 
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import re
 
 from matminer.utils.conversions import str_to_composition
@@ -103,41 +105,184 @@ df1["composition"] =df1["flatFormula"].transform(str_to_composition)
 df1.dropna(axis=1).head()
 
 
-# In[22]:
+# ### Drop samples without 'density'
+
+# In[126]:
 
 
-df1.shape
+# New DataFrame containing only samples with a density value
+df_dens = df1[df1.isnull()['Density'] == False]
 
 
-# In[20]:
+# In[127]:
 
 
-df1_feat = df1.copy()
+df_dens.shape
 
 
-# In[21]:
+# In[128]:
+
+
+# Plot occurrence of features of the reduced dataset
+df_dens.count().sort_values()[-17:].plot.barh()
+plt.show()
+
+
+# In[132]:
+
+
+# Drop all columns that contain less than 300 entries 
+# (crystallinity is only features left with NaN)
+df_dens = df_dens.dropna(axis=1, thresh=300)
+
+
+# ### Clean up columns
+
+# In[147]:
+
+
+df_dens.head()
+
+
+# We can drop some more columns that don't contain numerical or categorical data. Further, we 
+
+# In[152]:
+
+
+# Check if all density units are the same, in which case we don't need the units.
+df_dens['Density-units'].unique()
+
+
+# In[169]:
+
+
+# Determine number of polycrystalline / single crystal samples
+N_polyX = df_dens[df_dens['Crystallinity']=='Polycrystalline']['Crystallinity'].shape
+N_singleX = df_dens[df_dens['Crystallinity']=='Single Crystal']['Crystallinity'].shape
+print('Polycrystalline: {0}, Single crystal: {1}'.format(N_polyX, N_singleX))
+
+
+# In[168]:
+
+
+#Fill NaN values in crystallinity with polycrystalline:
+df_dens['Crystallinity'] = df_dens['Crystallinity'].fillna('Polycrystalline')
+
+
+# In[173]:
+
+
+# Check how density values cannot simply be transformed from string to int
+
+N_errors, N_total = 0, 0
+for entry in df_dens['Density']:
+    try:
+        pd.Series([entry]).astype(float)
+    except:
+        N_errors +=1
+        print(entry)
+    finally:
+        N_total +=1
+
+print('{0} errors in {1} samples'.format(N_errors, N_total))
+
+
+
+
+
+# In[170]:
+
+
+#Drop columns that don't contain numerical or categorical data
+df_dens.drop(['licenses','names','references','Density-units','Density-conditions','Chemical Family'], axis=1)
+
+
+# ### Add Additional Features with matminer
+
+# In[134]:
+
+
+df_dens.shape
+
+
+# In[135]:
+
+
+df_dens_add = df_dens.copy()
+
+
+# In[136]:
 
 
 # Add features with matminer
 ep_feat = ElementProperty.from_preset(preset_name="magpie")
-df1_feat = ep_feat.featurize_dataframe(df1_feat, col_id="composition", ignore_errors=True)
+df_dens_add = ep_feat.featurize_dataframe(df_dens_add, col_id="composition", ignore_errors=True)
 
 
-# In[23]:
+# In[137]:
 
 
-df1_feat.shape
+df_dens_add.shape
 
 
-# In[24]:
+# In[142]:
 
 
 # List of the new columns
-list(set(df1_feat.columns) ^ set(df1))
+list(set(df_dens_add.columns) ^ set(df_dens.columns))
 
 
-# In[25]:
+# In[144]:
 
 
-df1_feat['avg_dev MeltingT'].head()
+df_dens_add['avg_dev Column'].head()
+
+
+# ### Which features are numerical?
+
+# In[47]:
+
+
+df1_feat.dtypes[df1_feat.dtypes!='float64']
+
+
+# In[50]:
+
+
+# Exploring a non-scalar feature
+df1_feat[['Thermal Expansion','Thermal Expansion-conditions','Thermal Expansion-units']].dropna()
+
+
+# In[62]:
+
+
+df1_feat.loc[89,'Thermal Expansion-conditions']
+
+
+# In[61]:
+
+
+df1_feat.loc[89,'chemicalFormula']
+
+
+# In[66]:
+
+
+df1_feat['Compressive Strength-minimum'].dropna()
+
+
+# In[77]:
+
+
+df.count().hist(bins=1000)
+plt.xlim(0,100)
+plt.show()
+
+
+# In[71]:
+
+
+plt.figure(figsize=(8,16))
+df.count().sort_values()[-50:-8].plot.barh()
+plt.show()
 
